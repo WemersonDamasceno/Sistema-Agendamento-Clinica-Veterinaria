2364588d49dde66d2cbdd5428b3a1f8ed8984316
Wemerson Damasceno <wemerson_damasceno@yahoo.com>
2021-09-04 15:50:07 -0300

    Criando os arquivos do back e do front

---
+++ clinica-veterinaria/back-end/apirest/src/main/java/com/clinica/apirest/models/Cliente.java
@@ -0,0 +1,53 @@
+package com.clinica.apirest.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_CLIENTE")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	private String nome;
+	private String email;
+	private int cpf;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+	
+	
+	
+}
